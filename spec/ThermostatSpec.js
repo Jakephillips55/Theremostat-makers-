65ba21425c58f81bce51064f93f3a8e7a99c8d66
Jakephillips55 <Jakep629@gmail.com>
2020-01-22 13:52:50 +0000

    first 3 user stories done

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -10,4 +10,15 @@ describe('Thermostat', function() {
     it('starts at standard temp', function(){
         expect(thermostat.temp).toEqual(20); 
     }); 
-}); 
+
+    describe('raise temp', function( ) { 
+        it('raises the temp by 1', function() {
+        expect(thermostat.raiseTemp()).toEqual(21);
+    });
+    describe('lower temp', function() {
+        it('lowers the temp by 1', function(){
+            expect(thermostat.dropTemp()).toEqual(19);
+        });
+        })
+    });
+});  
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,5 +1,14 @@
 'use strict'; 
 
+
 function Thermostat(){
     this.temp = 20; 
 }
+    Thermostat.prototype.raiseTemp = function(){
+
+        return this.temp += 1     
+}
+    Thermostat.prototype.dropTemp = function(){
+
+        return this.temp -= 1
+    }
