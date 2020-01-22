94c7ce1f749f2582b27763a53460a7ddedd435f0
nicolasraffray <nraffray@gmail.com>
2020-01-22 16:13:25 +0000

    business logic refactor

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -8,61 +8,83 @@ describe('Thermostat', function() {
     }); 
 
     it('starts at standard temp', function(){
-        expect(thermostat.temp).toEqual(20); 
+        expect(thermostat.getCurrenttemp()).toEqual(20); 
     }); 
 
     describe('Controls Temp', function(){
    
+        /// THIS NEEDS ANOTHER IT FOR CHECKING IN RANGE/// 
         it('raises the temp by 1', function() {
-        expect(thermostat.raiseTemp()).toEqual(21);
-    });
+            expect(thermostat.raiseTemp()).toEqual(21);
+        });
     
         it('lowers the temp by 1', function(){
             expect(thermostat.dropTemp()).toEqual(19);
         });
-        })
+    
+        
         it('has max temp of 32', function(){ 
             thermostat.ecoOff();
-            expect(thermostat.maxTemp()).toEqual(32);
+            expect(thermostat.MAX_TEMP).toEqual(32);
         })
     
-            it('has a min temp', function(){
-                expect(thermostat.minTemp()).toEqual(10);
+        it('has a min temp', function(){
+            expect(thermostat.MIN_TEMP).toEqual(10);
         }); 
+
         it('can be reset to stdn temp', function(){
             thermostat.maxTemp();
             // setTimeout(thermostat, 1000);
             expect(thermostat.reset()).toEqual(20); 
         });
+    })
+
     describe('Eco mode', function(){
+        
         it('checks if power saving mode is on', function(){
             thermostat.pwrSave(); 
             expect(thermostat.eco).toEqual(true);
         });
+        
         it('wont let temp over 25', function (){
             thermostat.pwrSave();
-            expect(thermostat.maxTemp()).toEqual(25); 
+            expect(thermostat.MAX_TEMP).toEqual(25); 
         });
+        
         it('can be turned off', function(){
             thermostat.ecoOff();
             expect(thermostat.eco).toBeFalsy(); 
         });
         
     }); 
+
+
     describe('Usage data', function(){
         it('Tells user if usage is high', function(){
             thermostat.ecoOff(); 
-            thermostat.maxTemp(); 
+            thermostat.temp = thermostat.MAX_TEMP; 
             expect(thermostat.queryUsage()).toEqual('High usage');
         })
         it('Tells user if usage is medium', function(){
             expect(thermostat.queryUsage()).toEqual('Medium usage');
         });
         it('Tells user if usage is low', function(){
-            thermostat.minTemp();
+            thermostat.temp = thermostat.MIN_TEMP;
             console.log(thermostat.temp)
             expect(thermostat.queryUsage()).toEqual('Low usage'); 
         });
     });
+
+    // describe('range', function(){
+    //     it('checks user is in temp range lower bound', function(){
+    //         thermostat.minTemp
+    //         expect(function(){thermostat.dropTemp()}).toThrowError("Out of range")
+    //     });
+
+    //     it('check user is in tem range upper bound', function(){
+    //         thermostat.maxTemp
+    //         expect(function(){thermostat.raiseTemp()}).toThrowError("Out of range")
+    //     })
+    // });
     
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -2,42 +2,47 @@
 
 
 function Thermostat(){
-    this.eco = true
+    this.eco = true;
     this.temp = 20; 
+    this.MAX_TEMP = 25
+    this.MIN_TEMP = 10
 }
 
-    Thermostat.prototype.raiseTemp = function(){
+Thermostat.prototype.getCurrenttemp = function(){
+    return this.temp
+}
 
-        return this.temp += 1     
-    }
-    Thermostat.prototype.dropTemp = function(){
+Thermostat.prototype.raiseTemp = function(){
+    if(this.getCurrenttemp >= this.MAX_TEMP){ return "Out of Range"}
 
-        return this.temp -= 1
-    }
-    Thermostat.prototype.minTemp = function(){
-        return this.temp = 10 
-    }
-    Thermostat.prototype.pwrSave = function(){
-        this.eco = true 
-    }
-    Thermostat.prototype.ecoOff = function(){
-        this.eco = false
-    }
-    Thermostat.prototype.reset = function(){
-        return this.temp = 20; 
-    }
-    Thermostat.prototype.maxTemp = function(){
-        if(this.eco === true){return this.temp = 25}
-        else {
-            return this.temp = 32 
-        }
-    }
-    Thermostat.prototype.queryUsage = function(){
-        if (this.temp <= 18) {
-           return 'Low usage'
-        } else if (this.temp <= 25){
-            return 'Medium usage'
-        } else {
-            return 'High usage'
-        } 
-    }
+    return this.temp += 1     
+}
+Thermostat.prototype.dropTemp = function(){
+    if(this.getCurrenttemp <= this.MIN_TEMP){ return "Out of Range"}
+    return this.temp -= 1
+}
+
+Thermostat.prototype.pwrSave = function(){
+    this.eco = true 
+}
+
+Thermostat.prototype.ecoOff = function(){
+    this.eco = false
+    this.maxTemp()
+}
+Thermostat.prototype.reset = function(){
+    return this.temp = 20; 
+}
+Thermostat.prototype.maxTemp = function(){
+    if(this.eco === false){this.MAX_TEMP = 32}
+}
+
+Thermostat.prototype.queryUsage = function(){
+    if (this.temp <= 18) {
+       return 'Low usage'
+    } else if (this.temp <= 25){
+        return 'Medium usage'
+    } else {
+        return 'High usage'
+    };
+};
